dfc5b3b8fa2f40ff71652e8536610f16749322c6
cperez1012 <perezchristian1012@gmail.com>
2020-08-28 19:33:45 -0400

    seed data in seeds.rb for User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.new(username: 'cperez', email: 'perezchristian@gmail.com', password: '1234', image_url: 'https://scontent-lga3-1.cdninstagram.com/v/t51.2885-19/s320x320/12142387_725025870974201_1307390926_a.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_ohc=VJriPRRgHPIAX-J53M-&oh=d735ff3aea02f42a36a79f4166be193d&oe=5F7215EC')
