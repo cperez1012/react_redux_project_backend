b540282ee99456f07b824ccbcb2d8bf577d59f07
cperez1012 <perezchristian1012@gmail.com>
2020-08-29 01:03:30 -0400

    modified users controller to include index, new, create and user_params. Also added api/v1/lists_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::UsersController < ApplicationController
+    
+    def index
+        users = User.all
+    end
+
+    def new
+       user = User.new 
+    end
+
+    def create
+        user = User.new(user_params)
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
