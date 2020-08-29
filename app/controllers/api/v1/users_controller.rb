6257c9210f29417e69a77eb66a2aea237a9892db
cperez1012 <perezchristian1012@gmail.com>
2020-08-29 02:59:39 -0400

    modified index, create, update, destroy, find, and private methods set_user and user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,18 +1,47 @@
 class Api::V1::UsersController < ApplicationController
-    
-    def index
-        users = User.all
-    end
+    before_action :set_user, only: [:show, :update]
 
-    def new
-       user = User.new 
+    def index
+        # @users = User.all
+        @users = User
+        render json: @users
     end
 
     def create
-        user = User.new(user_params)
+        @user = User.new(user_params)
+        if @user.valid? && @user.save
+            render json: @user
+        else
+            render json: @user.errors, status: 400
+        end
+    end
+
+    def update
+        if @user.update(user_params)
+            render json: @user
+        else
+            render json: @user.errors, status: 400
+        end
+    end
+
+    def destroy
+        @user.destroy
+    end
+
+    def find
+        @user = User.find_by(email: params[:user][:email])
+        if @user
+            render json: @user
+        else
+            @user.errors.full_messages
+        end
     end
 
     private
+
+    def set_user
+        @user = User.find_by(id: params[:id])
+    end
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show] do
-        resources :lists, only: [:index]
-        resources :fighters, only: [:index, :create]
+      resources :users do
+        resources :lists, only: [:index] do
+          resources :fighters, only: [:index, :create]
+        end
       end
     end
   end
