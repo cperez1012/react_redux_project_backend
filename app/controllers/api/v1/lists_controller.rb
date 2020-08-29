d440a83478802547e95cf5e7a1f3cdeda93f8a0a
cperez1012 <perezchristian1012@gmail.com>
2020-08-29 11:41:20 -0400

    included index and create method within ListController

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,4 +1,17 @@
 class Api::V1::ListsController < ApplicationController
 
+    def index
+        @lists = List.all
+        render json: @lists
+    end
 
+    def create
+        @list = List.new(list_params)
+        if @list.save
+            render json: @list
+        else
+            render json: @list.errors, status: 400
+        end
+    end
+    
 end
