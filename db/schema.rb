ab5142c089775269faf967ac9b440d0aff12b8be
cperez1012 <perezchristian1012@gmail.com>
2020-08-28 21:05:59 -0400

    added foreign key for list and fighter association and list_id and index on fighters table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_29_010140) do
+ActiveRecord::Schema.define(version: 2020_08_29_010414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2020_08_29_010140) do
     t.integer "ko"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "list_id"
+    t.index ["list_id"], name: "index_fighters_on_list_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -51,4 +53,5 @@ ActiveRecord::Schema.define(version: 2020_08_29_010140) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "fighters", "lists"
 end
