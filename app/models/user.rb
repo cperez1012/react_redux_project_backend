c52fa57b5533d0e8c6b7c27b8356a5b9912afbb1
cperez1012 <perezchristian1012@gmail.com>
2020-08-29 00:37:28 -0400

    created api/v1/users_controller, updated list and user models for association, updated seed.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    # has_secure_password
-    # has_many :lists
-    # has_many :fighters, through: :lists
+    has_secure_password
+    has_many :lists
+    has_many :fighters, through: :lists
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.new(username: 'cperez', email: 'perezchristian@gmail.com', password_digest: '1234', image_url: 'https://scontent-lga3-1.cdninstagram.com/v/t51.2885-19/s320x320/12142387_725025870974201_1307390926_a.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_ohc=VJriPRRgHPIAX-J53M-&oh=d735ff3aea02f42a36a79f4166be193d&oe=5F7215EC')
+User.create(username: 'cperez', email: 'perezchristian@gmail.com', password_digest: '1234', image_url: 'https://scontent-lga3-1.cdninstagram.com/v/t51.2885-19/s320x320/12142387_725025870974201_1307390926_a.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_ohc=VJriPRRgHPIAX-J53M-&oh=d735ff3aea02f42a36a79f4166be193d&oe=5F7215EC', bio: 'I enjoy watching the sport of boxing!')
 
 favorite = List.create(title: 'Favorite Fighters')
 
