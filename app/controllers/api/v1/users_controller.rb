cdd74c3ac40efc982010b74329d9486edbeeee30
cperez1012 <perezchristian1012@gmail.com>
2020-08-30 06:02:03 -0400

    installed gem rack-cors and commented cors.rb within config/initializers folder

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UsersController < ApplicationController
     def index
         # @users = User.all
         @users = User.all
-        render json: @users
+        render json: UserSerializer.new(@users)
     end
 
     def show
