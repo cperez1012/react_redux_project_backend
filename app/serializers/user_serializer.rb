32ba0ac65c15f77f877497beab44087b6b924d5b
cperez1012 <perezchristian1012@gmail.com>
2020-08-30 05:47:47 -0400

    added attributes to User serializer as well as the associations to list and fighter

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,6 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes 
+  attributes :username, :email, :password_digest, :image_url, :bio
+  has_many :lists
+  has_many :fighters, through: :lists
 end
