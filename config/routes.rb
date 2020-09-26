0b64e074f4fd0b74d1ec2c98764ba7bc4f38e6b6
cperez1012 <perezchristian1012@gmail.com>
2020-09-25 22:40:29 -0400

    added delete logout route, updated sessions controller to include destroy method, removed lines not needed in user model file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   post '/api/v1/login', to: 'api/v1/sessions#create'
+  delete '/api/v1/logout', to: 'api/v1/sessions#destroy'
   get '/api/v1/get_current_user', to: 'api/v1/sessions#get_current_user'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
