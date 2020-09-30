d461593e0731fb60348c84d314de0b17eda8214b
cperez1012 <perezchristian1012@gmail.com>
2020-09-30 13:45:58 -0400

    added signup route for users to create account

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
   post '/api/v1/login', to: 'api/v1/sessions#create'
+  post '/api/v1/signup', to: 'api/v1/users#create'
   delete '/api/v1/logout', to: 'api/v1/sessions#destroy'
   get '/api/v1/get_current_user', to: 'api/v1/sessions#get_current_user'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :create, :show]
+      resources :users do
+        resources :lists, only: [:index]
+
       
       # post '/login', to: 'auth#create'
       # get '/profile', to: 'users#profile' 
       # do
         # resources :lists, only: [:index]
         # resources :fighters, only: [:index, :create]
-      # end
+      end
     end
   end
 end 
