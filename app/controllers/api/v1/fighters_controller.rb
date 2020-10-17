c31423cc4ba8f3a8087b3589655c50a0d2f5ad95
cperez1012 <perezchristian1012@gmail.com>
2020-10-17 11:51:06 -0400

    created show, update and destory methods for fighter, created set_fighter and list it as before action

--- app/controllers/api/v1/fighters_controller.rb
+++ app/controllers/api/v1/fighters_controller.rb
@@ -1,24 +1,56 @@
 class Api::V1::FightersController < ApplicationController
+  # before_action :set_fighter, only: [:show, :update, :destroy]
 
     def index
-        @fighters = Fighter.all
-        options = {
-          #include associated List
-          include: [:list]
+      if logged_in?
+        # @fighters = Fighter.all
+        @fighters = current_user.fighters
+        # binding.pry
+        # options = {
+        #   #include associated List
+        #   include: [:list]
+        # }
+        # render json: FighterSerializer.new(@fighters, options)
+        render json: FighterSerializer.new(@fighters)
+      else
+        render json: {
+          error: "You must be logged in to see your chosen fighters"
         }
-        render json: FighterSerializer.new(@fighters, options)
+      end
+    end
+
+    def show
+      render json: @fighter
     end
 
     def create
-        @fighter = Fighter.new(fighter_params)
+        # @fighter = Fighter.new(fighter_params)
+        @fighter = current_user.fighters.build(fighter_params)
+        # binding.pry
         if @fighter.save
-          render json: FighterSerializer.new(@fighter), status: :accepted
+          render json: FighterSerializer.new(@fighter).serialized_json, status: :accepted
         else
           render json: {errors: @fighter.errors.full_messages}, status: :unprocessible_entity
         end
     end
 
+    def update
+      if @fighter.update(fighter_params)
+        render json: FighterSerializer.new(@fighter), status: :ok
+      else
+        render json: @fighter.errors, status: :unprocessible_entity
+      end
+    end
+
+    def destroy
+      @fighter.destroy
+    end
+
     private
+
+    def set_fighter
+      @fighter = Fighter.find(params[:id])
+    end
 
     def fighter_params
         params.require(:fighter).permit(:name, :alias, :nationality, :division, :stance, :height, :reach, :status, :champion, :win, :loss, :draw, :ko, :list_id)
