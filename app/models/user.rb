10f0c8f940dc9ee258a262b0d5e2a7a54ea0f047
cperez1012 <perezchristian1012@gmail.com>
2020-10-26 10:24:00 -0400

    removed has many fighters through lists and kept just has many fighters for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     
     has_secure_password
     has_many :lists, dependent: :destroy
-    has_many :fighters, through: :lists
+    has_many :fighters
 
     validates :email, uniqueness: { case_sensitive: false }
 
