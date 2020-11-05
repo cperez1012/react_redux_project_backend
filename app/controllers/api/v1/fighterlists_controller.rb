72620c6ed5914f72e4e50071796cd20327392747
cperez1012 <perezchristian1012@gmail.com>
2020-11-05 10:34:26 -0500

    WIP, working on figuring out issues as to why fighters data does not pass to the frontend

--- app/controllers/api/v1/fighterlists_controller.rb
+++ app/controllers/api/v1/fighterlists_controller.rb
@@ -1,22 +1,13 @@
 class Api::V1::FighterlistsController < ApplicationController
 
-    def index
-        @fighterlists = Fighterlists.all
+    def create
+        fl = Fighterlist.new(list_id: params[:list_id], fighter_id: params[:fighter_id])
+        if fl.save
+            fighter = Fighterlist.find(params[:fighter_id])
+            render json: fighter
+        else
+            render json: {errors "Error adding fighter to list"}
+        end
     end
 
-    # def show
-
-    # end
-
-    # def create
-    
-    # end
-
-    # def update
-    
-    # end
-
-    # def destroy
-
-    # end
 end
--- app/controllers/api/v1/fighters_controller.rb
+++ app/controllers/api/v1/fighters_controller.rb
@@ -5,37 +5,37 @@ class Api::V1::FightersController < ApplicationController
     def index
       # Needs to be array of fighters in db
         # binding.pry
-        @fighters = Fighter.all
+        @fighters = Fighter.all.sort_by{ |f| f.name }
 
         render json: FighterSerializer.new(@fighters)
     end
 
-    def show
-      render json: @fighter
-    end
+    # def show
+    #   render json: @fighter
+    # end
 
-    def create
-      # Use find by or create by to prevent duplicates
-        @fighter = Fighter.new(fighter_params)
-        # binding.pry
-        if @fighter.save
-          render json: FighterSerializer.new(@fighter).serialized_json, status: :accepted
-        else
-          render json: {errors: @fighter.errors.full_messages}, status: :unprocessible_entity
-        end
-    end
+    # def create
+    #   # Use find by or create by to prevent duplicates
+    #     @fighter = Fighter.find_or_create_by(fighter_params)
+    #     # binding.pry
+    #     if @fighter.save
+    #       render json: FighterSerializer.new(@fighter).serialized_json, status: :accepted
+    #     else
+    #       render json: {errors: @fighter.errors.full_messages}, status: :unprocessible_entity
+    #     end
+    # end
 
-    def update
-      if @fighter.update(fighter_params)
-        render json: FighterSerializer.new(@fighter), status: :ok
-      else
-        render json: @fighter.errors, status: :unprocessible_entity
-      end
-    end
+    # def update
+    #   if @fighter.update(fighter_params)
+    #     render json: FighterSerializer.new(@fighter), status: :ok
+    #   else
+    #     render json: @fighter.errors, status: :unprocessible_entity
+    #   end
+    # end
 
-    def destroy
-      @fighter.destroy
-    end
+    # def destroy
+    #   @fighter.destroy
+    # end
 
     private
 
@@ -44,7 +44,7 @@ class Api::V1::FightersController < ApplicationController
     end
 
     def fighter_params
-      params.require(:fighter).permit(:name, :alias, :nationality, :division, :stance, :height, :reach, :status, :champion, :win, :loss, :draw, :ko, :list_id)
+      params.require(:fighter).permit(:name, :alias, :nationality, :division, :stance, :height, :reach, :status, :champion, :win, :loss, :draw, :ko)
     end
 
 end
