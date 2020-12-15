aecb46514d4b7d6f88bfa42e9b1a32d2e5755d03
cperez1012 <perezchristian1012@gmail.com>
2020-12-15 07:38:48 -0500

    added upvote to schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_170212) do
+ActiveRecord::Schema.define(version: 2020_12_02_031001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_10_28_170212) do
     t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "upvote"
     t.index ["list_id"], name: "index_fighters_on_list_id"
   end
 
