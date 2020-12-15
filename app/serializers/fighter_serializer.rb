64b5bb0a45833500b9c944b1eb789ba70e65f00d
cperez1012 <perezchristian1012@gmail.com>
2020-12-15 07:40:34 -0500

    updated fighter serializer to include upvote

--- app/serializers/fighter_serializer.rb
+++ app/serializers/fighter_serializer.rb
@@ -1,5 +1,5 @@
 class FighterSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :alias, :nationality, :division, :stance, :height, :reach, :status, :champion, :win, :loss, :draw, :ko, :list_id, :list
+  attributes :name, :alias, :nationality, :division, :stance, :height, :reach, :status, :champion, :win, :loss, :draw, :ko, :list_id, :list, :upvote
   belongs_to :list
 end
