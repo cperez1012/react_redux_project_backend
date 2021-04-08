7b782d86d5dbd6385dba9ff86e9bc1691187a733
cperez1012 <perezchristian1012@gmail.com>
2021-04-08 19:56:38 -0400

    removed uncommented lines from lists controller

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -6,8 +6,6 @@ class Api::V1::ListsController < ApplicationController
 
             @lists = current_user.lists
 
-            # render json: List.new(@lists)
-
             render json: ListSerializer.new(@lists,{include: [:fighters]}).serialized_json
         else
             render json: {
@@ -22,11 +20,10 @@ class Api::V1::ListsController < ApplicationController
     end
 
     def create
-        # @list = List.new(list_params)
+
         @list = current_user.lists.build(list_params)
         if @list.save
 
-            # render json: List.new(@list, status: :created)
             render json: ListSerializer.new(@list), status: :created
         else
             error_resp = {
@@ -38,7 +35,7 @@ class Api::V1::ListsController < ApplicationController
 
     def update
         if @list.update(list_params)
-            # render json: List.new(@list, status: :ok)
+      
             render json: ListSerializer.new(@list), status: :ok
         else
             error_resp = {
