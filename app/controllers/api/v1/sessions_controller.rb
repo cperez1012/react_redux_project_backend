910ecbc081a6faa6698b3aa63ca1316d9bb38b81
cperez1012 <perezchristian1012@gmail.com>
2021-04-08 19:57:45 -0400

    removed uncommented lines from sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,12 +2,10 @@ class Api::V1::SessionsController < ApplicationController
 
     def create
         @user = User.find_by(username: params[:session][:username])
-        # binding.pry
 
         if @user && @user.authenticate(params[:session][:password])
             session[:user_id] = @user.id
             render json: UserSerializer.new(@user)
-
         else
             render json: {
                 error: "Invalid Credentials"
@@ -18,7 +16,6 @@ class Api::V1::SessionsController < ApplicationController
 
     def get_current_user
         if logged_in?
-            # render json: current_user
             render json: UserSerializer.new(current_user)
         else
             render json: {
