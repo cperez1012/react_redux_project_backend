c14a4f117cc38191034fda623055d233d7e9a98d
cperez1012 <perezchristian1012@gmail.com>
2021-04-08 20:00:09 -0400

    removed uncommented lines and changed imageurl attribute in user params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,38 +1,33 @@
 require 'pry'
 class Api::V1::UsersController < ApplicationController
-    # skip_before_action :authorized, only: [:create]\
+
     before_action :set_user, only: [:show, :update, :destroy]
 
     # GET /users
     def index
         @users = User.all
         render json: @users
-        # render json: UserSerializer.new(@users)
     end
 
     def show
-        # @user = User.find(params[:id])
+
         user_json = UserSerializer.new(@user).serialized_json
-        # binding.pry
+
         render json: user_json
     end
 
     def create
         @user = User.new(user_params)
-        # @user.password = params[:password]
+
         if @user.save
             session[:user_id] = @user.id
-        # if @user.valid?
-            # @token = encode_token(user_id: @user.id)
-            # render json: { user: UserSerializer.new(@user) }, status: :created
-            # render json: @user, status: :created, location: @user
+
             render json: UserSerializer.new(@user), status: :created
         else
             resp = {
                 error: @user.errors.full_messages.to_sentence
               }
               render json: resp, status: :unprocessable_entity
-            # render json: { error: 'failed to create user' }, status: :not_acceptable
         end
     end
 
@@ -55,6 +50,6 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def user_params
-        params.require(:user).permit(:username, :email, :password, :bio, :image_url)
+        params.require(:user).permit(:username, :email, :password, :bio, :imageurl)
     end
 end
