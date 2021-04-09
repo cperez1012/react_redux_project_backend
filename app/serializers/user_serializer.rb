a61a0247826c7b136c1e6054dc2b751d2516a8d2
cperez1012 <perezchristian1012@gmail.com>
2021-04-08 20:02:46 -0400

    changed imageurl attribute within user serializer file

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :username, :email, :image_url, :bio
+  attributes :username, :email, :imageurl, :bio
   has_many :lists, dependent: :destroy
   has_many :fighters, through: :lists
 end
