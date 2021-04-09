2b101cbedf7b96ae96f99c8b352175db0255f43e
cperez1012 <perezchristian1012@gmail.com>
2021-04-08 20:01:56 -0400

    uncommented validation for email format in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
     has_many :fighters, through: :lists
 
     validates :email, uniqueness: true
+    # validates_format_of  :email, :with => /^[\+A-Z0-9\._%-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i
+
 end
