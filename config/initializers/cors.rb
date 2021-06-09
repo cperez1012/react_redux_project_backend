c5b9c5180a61d1e7fcd1b2f9e079049028f846d2
cperez1012 <perezchristian1012@gmail.com>
2021-06-09 12:30:28 -0400

    added specific frontend url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://compassionate-engelbart-8fa2cb.netlify.app/'
 
     resource '*',
       headers: :any,
